e89998cba797d1b08754b7ced6c48cdbd02e8948
Miguel Hernández <>
2021-05-16 15:42:40 -0500

    Tarea4Ejercicio1

---
+++ src/tarea4ejercicio1/Tarea4Ejercicio1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea4ejercicio1;
+
+/**
+ *
+ * @author ROCIO
+ */
+public class Tarea4Ejercicio1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                int n = 1;
+        for (int i=1;i<=103;i++){
+        n=n*i;
+        }
+        System.out.println("El factorial de 4 es "+ n);
+    }
+    
+}
